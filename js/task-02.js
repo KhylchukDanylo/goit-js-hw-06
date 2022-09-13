a075420012a45d1fe5527aff0bea5cdd27c5a4d4
Danilo <hilcukdanilo@gmail.com>
2022-09-13 13:15:15 +0200

    add task 2 and 3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs"];
+const arrayListIngredient = [];
+for (let i = 0; i < ingredients.length; i++) {
+  const listIngredient = document.createElement("li");
+  listIngredient.classList = "item";
+  listIngredient.textContent = `${ingredients[i]}`;
+  arrayListIngredient.push(listIngredient);
+}
+const listItem = document.querySelector("#ingredients");
+listItem.append(...arrayListIngredient);
