31c95db5c5dc01a2cee0ad343a9b36278b6090c1
Danilo <hilcukdanilo@gmail.com>
2022-09-15 21:07:26 +0200

    +add

--- js/task-02.js
+++ js/task-02.js
@@ -2,7 +2,7 @@ const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs"];
 const arrayListIngredient = [];
 for (let i = 0; i < ingredients.length; i++) {
   const listIngredient = document.createElement("li");
-  listIngredient.classList = "item";
+  listIngredient.classList.add("item");
   listIngredient.textContent = `${ingredients[i]}`;
   arrayListIngredient.push(listIngredient);
 }
